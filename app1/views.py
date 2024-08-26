52c9afab52139ed9c4f97daee5c3d3a3238d0a54
larriag <luis.arriagada.cerda@gmail.com>
2024-08-26 03:09:04 -0400

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def mostrar(request):
-    return HttpResponse("Hola mundo, soy la app")
+    return HttpResponse("Hola mundo, soy la app pero modificada desde rama 1")
